6106e3696c2bba5270ce25740dc0b1036cdb1498
pberkhout <patrick.berkhout@trivento.nl>
2017-07-27 12:23:21 +0200

    sqlserver import

--- import.sh
+++ import.sh
@@ -16,7 +16,18 @@ mkdir -p ${DIR}/backups
 
 
 dc up -d --build database
-sleep 50
+sleep 20
 
-dc run --rm importer
-dc run --rm db-backup
+# Load data from NPR Database, save in /tmp/parkeererechten/export/output.csv
+docker-compose -p parkeerrechten run --rm sqlserverimporter db2csv \
+        -debug \
+        -server ${EXTERNAL_PARKEERRECHTEN_DATABASE_HOST}\
+        -user ${EXTERNAL_PARKEERRECHTEN_DATABASE_USER}  \
+        -password ${EXTERNAL_PARKEERRECHTEN_DATABASE_PASSWORD} \
+        -database ODP \
+        -query 'select TOP 10 * from VW_0363;'
+
+# Load data from objectstore 2016 (raw dumps)
+#dc run --rm importer
+
+#dc run --rm db-backup
