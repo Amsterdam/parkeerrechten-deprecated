b3b87bf8c2263570b1a3dad357a87fd6ebe70588
pberkhout <patrick.berkhout@trivento.nl>
2017-07-27 13:26:08 +0200

    jenkins

--- import.sh
+++ import.sh
@@ -21,9 +21,9 @@ sleep 20
 # Load data from NPR Database, save in /tmp/parkeererechten/export/output.csv
 docker-compose -p parkeerrechten run --rm sqlserverimporter db2csv \
         -debug \
-        -server ${EXTERNAL_PARKEERRECHTEN_DATABASE_HOST}\
-        -user ${EXTERNAL_PARKEERRECHTEN_DATABASE_USER}  \
-        -password ${EXTERNAL_PARKEERRECHTEN_DATABASE_PASSWORD} \
+        -server ljccjhajab.database.windows.net\
+        -user usr_0363  \
+        -password ${NPR_DATABASE_PASSWORD} \
         -database ODP \
         -query 'select TOP 10 * from VW_0363;'
 
